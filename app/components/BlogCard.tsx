c131a1f904fe373f5b71af85c79ad0ee544f223a
mauluddin12z <mauludindayat@gmail.com>
2023-06-20 22:24:43 +0700

    fix

--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -11,7 +11,7 @@ import TextEllipsis from "../utils/TextEllipsis";
 
 const getBlogById = async (id: number) => {
   const res = await axios.get(
-    `${process.env.NEXT_PUBLIC_MY_BACKEND_URL}blogs/${id}`
+    `${process.env.NEXT_PUBLIC_MY_BACKEND_URL}blog/${id}`
   );
   return res.data;
 };
