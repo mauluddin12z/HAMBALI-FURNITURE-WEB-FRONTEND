0526a6706d8ae270b445aec76370e9b9a15a7e59
Muhammad Hidayat Mauluddin <mauludindayat@gmail.com>
2025-07-02 17:42:28 +0700

    update PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="manifest" href="/manifest.json" />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
           rel="stylesheet"
